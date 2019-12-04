8255cf5625b46ccc4b2a123b8937a4268b32a264
Gupta, Medhavi <medhavi.gupta@kronos.com>
2019-12-04 22:24:54 +0530

    removing double requests to redis

--- slidingWindowCounter.js
+++ slidingWindowCounter.js
@@ -2,58 +2,56 @@ var redis = require('redis');
 var redisClient = redis.createClient();
 
 module.exports = (req,res,next) => {
-    redisClient.exists(req.headers.user,(err,reply) => {
+    redisClient.get(req.headers.user,(err,redisResponse) => {
         if(err){
             console.log("Problem with redis");
             system.exit(0);
         }
-        if(reply === 1) {
-            redisClient.get(req.headers.user,(err,redisResponse) => {
-                let data = JSON.parse(redisResponse);
-				let d = new Date();
-				let currentTime = d.getTime();
-				let aMinuteAgo = currentTime - 60000;
-				console.log("currentTime : "+currentTime+" aMinuteAgo : "+aMinuteAgo);
+        if(redisResponse) {
+            let data = JSON.parse(redisResponse);
+            let d = new Date();
+            let currentTime = d.getTime();
+            let aMinuteAgo = currentTime - 6000;
+            console.log("currentTime : "+currentTime+" aMinuteAgo : "+aMinuteAgo);
 
-                let RequestCountPerMinutes = data.filter((item) => {
-					console.log(item);
-					if(item.requestTime < aMinuteAgo)
-					{
-						data.shift();
-						console.log("shifted the data "+item.counter);
-					}	
-                    return item.requestTime > aMinuteAgo;
-                })
-
-                let thresHold = 0;
-
-                RequestCountPerMinutes.forEach((item) => {
-                    thresHold = thresHold + item.counter;
-					console.log("increased thresHold, counter is "+ item.counter);
-                })
-                if(thresHold >= 5){
-                    return res.json({ "error" : 1,"message" : "throttle limit exceeded" })
+            let RequestCountPerMinutes = data.filter((item) => {
+                console.log(item);
+                if(item.requestTime < aMinuteAgo)
+                {
+                    data.shift();
+                    console.log("shifted the data "+item.counter);
                 }
-                else{
-                    let isFound = false;
-                    data.forEach(element => {
-                        if(element.requestTime) {
-                            isFound = true;
-                            element.counter++;
-                        }
-						console.log("found it. Threshold is "+thresHold);	
-                    });
-                    if(!isFound){
-                        data.push({
-                            requestTime : currentTime,
-                            counter : 1
-                        })
-						console.log("pushing...");
-                    }
-                    redisClient.set(req.headers.user,JSON.stringify(data));
-                    next();
-                }
+                return item.requestTime > aMinuteAgo;
             })
+
+            let thresHold = 0;
+
+            RequestCountPerMinutes.forEach((item) => {
+                thresHold = thresHold + item.counter;
+                console.log("increased thresHold, counter is "+ item.counter);
+            })
+            if(thresHold >= 3){
+                return res.json({ "error" : 1,"message" : "throttle limit exceeded" })
+            }
+            else{
+                let isFound = false;
+                data.forEach(element => {
+                    if(element.requestTime) {
+                        isFound = true;
+                        element.counter++;
+                    }
+                    console.log("found it. Threshold is "+thresHold);
+                });
+                if(!isFound){
+                    data.push({
+                        requestTime : currentTime,
+                        counter : 1
+                    })
+                    console.log("pushing...");
+                }
+                redisClient.set(req.headers.user,JSON.stringify(data));
+                next();
+            }
         }
         else{
             let data = [];
